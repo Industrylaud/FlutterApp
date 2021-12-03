3dc961d2eaf46ed044d9c345df37fc82df0aa3ce
industrylaud <mrfros22@gmail.com>
2021-12-03 19:16:32 +0100

    add image assets

--- lib/main.dart
+++ lib/main.dart
@@ -17,16 +17,7 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.deepPurpleAccent,
       ),
       body: Center(
-        child: Text(
-          'love',
-          style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            color: Colors.grey[600],
-            fontFamily: 'IndieFlower',
-          ),
-        ),
+        child: Image.asset('assets/space-2.jpg')
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {  },
